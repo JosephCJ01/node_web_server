685c10d4ca431a2de3e5f4b75fbcec40445d40e1
JosephCJ01 <brianchoqueji8@gmail.com>
2024-04-06 06:45:40 -0500

    make a web server where we serve a social network application of the little bird

---
+++ src/config/env.js
@@ -0,0 +1,12 @@
+// en esta carpeta el manejo de variables de entorno
+require('dotenv').config()
+const { get } = require('env-var')
+
+const envs = {
+    PORT: get('PORT').required().asPortNumber(),
+    PUBLIC_PATH: get('PUBLIC_PATH').default('public').asString()
+}
+
+module.exports = {
+    envs
+}
---
+++ src/server/server.js
@@ -0,0 +1,26 @@
+//en esta caprta se manejara el servidor
+const express = require('express')
+const path = require('path')
+
+const startServer = (options) => {
+    const { port, public_path = 'public'} = options
+    
+    const app = express()
+
+
+    //Para usar middlewares se usa la palabra use(express)
+    app.use(express.static(public_path)) //contenido estatico que ponemos disponible para que se uitilice
+
+    app.get('*',(req, res)=>{
+        const indexPath = path.join(__dirname + `../../../${public_path}/index.html`)
+        res.sendFile(indexPath)
+    })
+
+    app.listen(port,()=>{
+        console.log(`Escuchando en el puerto ${port}`)
+    })
+}
+
+module.exports = {
+    startServer
+}
